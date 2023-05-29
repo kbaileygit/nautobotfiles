177c15058181e42751deb29c29ecb8c3a5febb32
kbaileygit <40672814+kbaileygit@users.noreply.github.com>
2023-05-28 22:33:47 -0700

    Create will.py

---
+++ will.py
@@ -0,0 +1,11 @@
+from nautobot.extras.jobs import Job
+#from b import hello
+
+
+class WillSucceed(Job):
+    class Meta:
+        description = 'test'
+        commit_default = False
+
+    def run(self, data, commit):
+        return self.log_info(message=hello)
